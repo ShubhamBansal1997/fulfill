496764bd3f592e06ecca9286f537449c975d6178
Shubham Bansal <shubhambansal17@hotmail.com>
2020-10-02 03:41:11 +0530

    👌 shifted from the api approach to the websocket approach - added loader for the processing file time

--- src/pages/progressBar/index.js
+++ src/pages/progressBar/index.js
@@ -1,12 +1,12 @@
 import React, {Component} from "react";
 import Base from "../../layout/base";
 import Button from "@material-ui/core/Button";
-import api from "../../utils/api";
 import {withRouter} from "react-router";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import {Link} from "react-router-dom";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 function LinearProgressWithLabel(props) {
     return (
@@ -31,39 +31,52 @@ class ProgressBar extends Component {
             progressId: this.props.match.params.progressId,
             progress: 0,
             current: 0,
-            total: 0
+            total: 0,
+            ws: null
         }
     }
 
     componentDidMount() {
-        let taskStatus = setInterval(() => {
-            if (this.state.progress < 100) {
-                this.fetchData();
-            } else {
-                clearInterval(taskStatus);
-            }
-        }, 1000)
+        this.connect();
     }
 
-    fetchData() {
-        api
-            .get(`/celery-progress/${this.state.progressId}`)
-            .then((response) => {
-                let {percent, current, total} = response.progress
-                this.setState({progress: parseInt(percent), current: current, total: total})
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+
+    connect = () => {
+        let {progressId} = this.state
+        let ws = new WebSocket(`${process.env.REACT_APP_WS_URL}/ws/progress/${progressId}/`);
+        let that = this; // cache the this
+
+        // websocket onopen event listener
+        ws.onopen = () => {
+            ws.send(JSON.stringify({'type': 'check_task_completion'}));
+            that.setState({ws: ws});// clear Interval on on open of websocket connection
+        }
+
+        ws.onmessage = event => {
+            let data;
+            data = JSON.parse(event.data)
+            let {percent, current, total} = data.progress
+            this.setState({progress: parseInt(percent || 0), current: current || 0, total: total || 0})
+        }
+        ws.onerror = err => {
+            console.log(err);
+            ws.close();
+        }
     }
+
+    processingFile = () => (
+        <Typography variant="h4" gutterBottom>
+            Processing File <CircularProgress/>
+        </Typography>
+    )
 
     render() {
         let {progress, current, total} = this.state;
         return (
             <Base>
-                <Typography variant="h4" gutterBottom>
+                {progress === 0 ? this.processingFile() : <Typography variant="h4" gutterBottom>
                     Products {progress !== 100 ? `Uploading ${current}/${total}` : `Uploaded 🎉🎉🎉`}
-                </Typography>
+                </Typography>}
                 <LinearProgressWithLabel value={progress}/>
                 {progress === 100 ? <Button variant="contained" color="primary" component={Link} to="/">
                     Move to Products Page</Button> : null}
